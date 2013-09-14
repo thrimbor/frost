e2ff3f7163839d4a61672fa63224a11fe216be29
Stefan Schmidt <thrimbor.github@gmail.com>
2013-09-14 08:48:24 +0200

    * the qemu script is now smaller and attaches the monitor to stdio for debugging

--- build/qemu.sh
+++ build/qemu.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-if [ -f /usr/bin/kvm ]
-then
-	kvm -m 128 -boot d -cdrom frost.iso
-elif [ -f /usr/bin/qemu-system-i386 ]
-then
-	qemu-system-i386 -m 128 -boot d -cdrom frost.iso -no-reboot -no-shutdown
-else
-	qemu -m 128 -boot d -cdrom frost.iso
-fi
-
+qemu-system-i386 -m 128 -boot d -cdrom frost.iso -no-reboot -no-shutdown -monitor stdio
